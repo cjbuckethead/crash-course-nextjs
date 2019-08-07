3661ee449815b0fb04f44971716330d29b25666d
charles <charles.malata@baesystems.com>
2019-08-07 18:17:25 +0800

    WIP

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Fetch from "isomorphic-unfetch";
+import Layout from "../components/Layout";
+
+const Index = props => (
+  <Layout>
+    <div>
+      <h1>Welcome to BitzPrice</h1>
+      {props.bpi.time.updated}
+    </div>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+
+  return {
+    bpi: data
+  };
+};
+
+export default Index;
